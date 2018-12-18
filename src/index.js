5fe7796bf44b38df1f828f31b362cac6cf36d158
youling <youling0106@qq.com>
2018-12-18 22:17:07 +0800

    modify the way of imports sagas

--- src/index.js
+++ src/index.js
@@ -6,14 +6,17 @@ import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import Routes from 'routes'
 import Reducers from 'reducers'
-import Sagas from 'sagas'
+import shici from './sagas/shici'
 import 'styles/global-styles'
 import registerServiceWorker from 'utils/registerServiceWorker'
 
-const sagaMiddleware = createSagaMiddleware(Sagas)
+const sagaMiddleware = createSagaMiddleware(shici)
+const store = createStore(
+  Reducers,
+  applyMiddleware(sagaMiddleware)
+)
 render(
-  <Provider store={createStore(Reducers,
-    applyMiddleware(sagaMiddleware))}>
+  <Provider store={store}>
     <Routes />
   </Provider>,
   document.getElementById('root')
