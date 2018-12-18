cf07feb87a6c7626d9d9de2651d6639b8aaf07a7
youling <youling0106@qq.com>
2018-12-18 22:52:15 +0800

    change default reducer

--- src/reducers/shici.js
+++ src/reducers/shici.js
@@ -1,6 +1,7 @@
 import { CHANGE_SHICI_RES } from 'constants/ActionTypes'
 
-const oneShici = '东边日出西边雨，道是无晴却有晴'
+// const oneShici = '东边日出西边雨，道是无晴却有晴'
+const oneShici = '纵使晴明无雨色，入云深处亦沾衣'
 
 export default function shici(state = { oneShici }, action) {
   switch (action.type) {
